7f758886b9eeed415bea60851816659305e48e02
T.Hasegawa <t.hasegawa.853@ms.saitama-u.ac.jp>
2024-09-20 14:47:13 +0900

    パスの修正

--- designlab/__init__.py
+++ designlab/__init__.py
@@ -3,10 +3,10 @@
 
 # 自作のパッケージの設定ファイル
 
-from .hexapod_leg_range_calculator import HexapodLegRangeCalculator
-from .hexapod_leg_renderer import HexapodLegRenderer
-from .approximated_graph_renderer import ApproximatedGraphRenderer
-from .mouse_grid_renderer import MouseGridRenderer
-from .hexapod_range_of_motion import HexapodRangeOfMotion
-from .hexapod_leg_power import HexapodLegPower
-from .triangle_checker import TriangleChecker
+from designlab.hexapod_leg_range_calculator import HexapodLegRangeCalculator
+from designlab.hexapod_leg_renderer import HexapodLegRenderer
+from designlab.approximated_graph_renderer import ApproximatedGraphRenderer
+from designlab.mouse_grid_renderer import MouseGridRenderer
+from designlab.hexapod_range_of_motion import HexapodRangeOfMotion
+from designlab.hexapod_leg_power import HexapodLegPower
+from designlab.triangle_checker import TriangleChecker
